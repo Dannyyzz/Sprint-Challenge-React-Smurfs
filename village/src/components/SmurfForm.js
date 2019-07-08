e47dd072b3f094df189d6d0ca2eab518354e1468
Dannyyzz <dannyzz@live.com>
2019-07-07 22:23:22 -0400

    Small Formating

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -5,54 +5,58 @@ class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     };
   }
 
   addUpdateSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-    if (this.props.updating) {axios.put(`http://localhost:3333/smurfs/${this.props.match.params.id}`, this.state)
-    .then( res => {
-      this.props.updateState(res.data);
-      this.props.history.push('/')
-    })
-    .catch( err => console.log(err))
-    return
+    if (this.props.updating) {
+      axios.put(`http://localhost:3333/smurfs/${this.props.match.params.id}`,
+          this.state
+        )
+        .then(res => {
+          this.props.updateState(res.data);
+          this.props.history.push("/");
+        })
+        .catch(err => console.log(err));
+      return;
     } else {
-      axios.post('http://localhost:3333/smurfs', this.state)
-      .then(res => {
-        this.props.updateState(res.data);
-        this.props.history.push('/')
-      })
-      .catch(err => console.log(err))
-  }
+      axios
+        .post("http://localhost:3333/smurfs", this.state)
+        .then(res => {
+          this.props.updateState(res.data);
+          this.props.history.push("/");
+        })
+        .catch(err => console.log(err));
+    }
 
     this.setState({
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     });
-  }
+  };
 
   componentDidMount() {
     if (this.props.updating) {
       const id = this.props.match.params.id;
       const smurf = this.props.smurfs.find(smurf => `${smurf.id}` === id);
 
-       this.setState({
+      this.setState({
         name: smurf.name,
         age: smurf.age,
         height: smurf.height
-      })
+      });
     } else {
       this.setState({
-        name: '',
-        age: '',
-        height: ''
-      })
+        name: "",
+        age: "",
+        height: ""
+      });
     }
   }
 
@@ -82,7 +86,9 @@ class SmurfForm extends Component {
             value={this.state.height}
             name="height"
           />
-          <button type="submit">{this.props.update ? "Update" : "Add to Smurf Village!"}</button>
+          <button type="submit">
+            {this.props.update ? "Update" : "Add to Smurf Village!"}
+          </button>
         </form>
       </div>
     );

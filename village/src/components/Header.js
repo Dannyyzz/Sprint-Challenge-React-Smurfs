4e39e6a92615fac685cc6d8a6eb5b8ff345ed0ee
Dannyyzz <dannyzz@live.com>
2019-07-07 21:52:48 -0400

    Changed Link to NavLink

--- village/src/components/Header.js
+++ village/src/components/Header.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
  class Header extends Component {
   render() {
     return (
       <div>
         <h1>Smurfs!</h1>
-        <Link to="/smurfs">View Smurfs</Link>
-        <Link to="/form">Add Smurf</Link>
+        <hr></hr>
+        <NavLink to="/smurfs">View Smurfs </NavLink>
+        <NavLink to="/form">Add Smurf</NavLink>
       </div>
     )
   }
